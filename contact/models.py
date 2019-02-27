5276c9aedf46b042029e8ac3f974a65123f21f89
Selpisrimulyani <Selpimulyani25@gmail.com>
2019-02-27 21:07:56 +0700

    upgrade

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,8 @@ class contact(models.Model):
     Email = models.CharField(max_length=20)
     Twiiter = models.CharField(max_length=20)
     Github = models.CharField(max_length=20)
+    
+
 
     def __unicode__(self):
         return self.Email
